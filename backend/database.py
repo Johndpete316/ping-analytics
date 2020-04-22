161eed93a9f910276c7dbda8e1deaa57495c4985
John Peterson <johndpete316@gmail.com>
2020-04-22 16:33:58 -0400

    Moved python backend into backend folder
    Signed-off-by: John Peterson <johndpete316@gmail.com>

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def db_main(est, mdt, pt, ping_value, conn):    
+    c = conn.cursor()
+    init_db(c)
+    c.execute("INSERT INTO ping(est, mdt, pst, ping_value) VALUES (?, ?, ?, ?)", (est, mdt, pt, ping_value))
+    conn.commit()
+    conn.close()
+
+
+def init_db(c):
+    c.execute("CREATE TABLE IF NOT EXISTS ping(est TEXT, mdt TEXT, pst TEXT, ping_value REAL)")
+
