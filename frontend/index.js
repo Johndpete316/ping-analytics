6e4b0c2ebff234832d10cba4e42e2e742d3c2efb
John Peterson <johndpete316@gmail.com>
2020-04-22 23:50:17 -0400

    removed authentication (it was pointless)
    Signed-off-by: John Peterson <johndpete316@gmail.com>

--- frontend/index.js
+++ frontend/index.js
@@ -4,10 +4,6 @@
 const express = require("express")
 const path = require("path")
 
-const expressSession = require("express-session")
-const passport = require("passport")
-const Auth0Strategy = require("passport-auth0")
-
 require("dotenv").config()
 
 const authRouter = require("./auth")
@@ -26,70 +22,11 @@ let db = new sqlite3.Database('../ping-analytics.db', (err) => {
 const app = express()
 const port = process.env.PORT || "8000"
 
-// Session configuration
-
-const session = {
-    secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
-    cookie: {},
-    resave: false,
-    saveUninitialized: false
-  };
-  
-if (app.get("env") === "production") {
-    session.cookie.secure = true;
-}
-
-// Passport configuration
-
-const strategy = new Auth0Strategy(
-    {
-      domain: process.env.AUTH0_DOMAIN,
-      clientID: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL:
-        process.env.AUTH0_CALLBACK_URL || "http://localhost:8000/callback"
-    },
-    function(accessToken, refreshToken, extraParams, profile, done) {
-      /**
-       * Access tokens are used to authorize users to an API
-       * (resource server)
-       * accessToken is the token to call the Auth0 API
-       * or a secured third-party API
-       * extraParams.id_token has the JSON Web Token
-       * profile has all the information from the user
-       */
-      return done(null, profile);
-    }
-  );
-
 
 // App configuration
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 app.use(express.static(path.join(__dirname, "public")))
-
-app.use(expressSession(session))
-
-passport.use(strategy)
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.serializeUser((user, done) => {
-    done(null, user)
-})
-
-passport.deserializeUser((user, done) => {
-    done(null, user)
-})
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.isAuthenticated();
-    next();
-});
-  
-app.use("/", authRouter);
-
-app.use("/", authRouter);
 
 
 // Route Definitions
