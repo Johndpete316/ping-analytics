a8fd95496a7e6bdb9ee75026defa9f57e460c020
John Peterson <johndpete316@gmail.com>
2020-04-30 00:38:34 -0400

    streamlined ping database

--- frontend/dist/ping.dev.js
+++ frontend/dist/ping.dev.js
@@ -5,17 +5,8 @@ var tcpp = require("tcp-ping");
 var moment = require('moment-timezone');
 
 var _require = require('mongodb'),
-    MongoClient = _require.MongoClient; //date
+    MongoClient = _require.MongoClient; //configure mongoDB
 
-
-var time_utc = moment().tz("UTC").format('LTS');
-var date_utc = moment().tz("UTC").format('L');
-var time_est = moment().format('LTS');
-var date_est = moment().format('L');
-
-var _id = "".concat(date_est, " ").concat(time_est);
-
-console.log(_id); //configure mongoDB
 
 var _require2 = require("../config.json"),
     password = _require2.password,
@@ -26,43 +17,67 @@ var client = new MongoClient(uri, {
   useUnifiedTopology: true
 });
 var host = '51.161.117.73';
-tcpp.ping({
-  address: host,
-  port: 25677
-}, function _callee(err, data) {
-  return regeneratorRuntime.async(function _callee$(_context) {
+
+function main() {
+  var time_utc, date_utc, time_est, date_est, _id;
+
+  return regeneratorRuntime.async(function main$(_context2) {
     while (1) {
-      switch (_context.prev = _context.next) {
+      switch (_context2.prev = _context2.next) {
         case 0:
-          ping_data = {
-            "_id": _id,
-            "day": moment().format('ddd'),
-            "hour": moment().format('hh'),
-            "device": "the-beast",
-            "date_est": date_est,
-            "time_est": time_est,
-            "date_utc": date_utc,
-            "time_utc": time_utc,
-            "ip": data.address,
-            "ping_avg": data.avg,
-            "ping_max": data.max,
-            "ping_min": data.min
-          };
-          _context.next = 3;
-          return regeneratorRuntime.awrap(client.connect());
+          x = 5;
+          time_utc = moment().tz("UTC").format('LTS');
+          date_utc = moment().tz("UTC").format('L');
+          time_est = moment().format('LTS');
+          date_est = moment().format('L');
+          _id = "".concat(date_est, " ").concat(time_est);
+          tcpp.ping({
+            address: host,
+            port: 25677
+          }, function _callee(err, data) {
+            return regeneratorRuntime.async(function _callee$(_context) {
+              while (1) {
+                switch (_context.prev = _context.next) {
+                  case 0:
+                    console.log(_id);
+                    ping_data = {
+                      "_id": _id,
+                      "day": moment().format('ddd'),
+                      "hour": moment().format('hh'),
+                      "device": "the-beast",
+                      "date_est": date_est,
+                      "time_est": time_est,
+                      "date_utc": date_utc,
+                      "time_utc": time_utc,
+                      "ip": data.address,
+                      "ping_avg": data.avg,
+                      "ping_max": data.max,
+                      "ping_min": data.min
+                    };
+                    _context.next = 4;
+                    return regeneratorRuntime.awrap(client.connect());
 
-        case 3:
-          _context.next = 5;
-          return regeneratorRuntime.awrap(client.db("data").collection("ping-data").insertOne(ping_data));
+                  case 4:
+                    _context.next = 6;
+                    return regeneratorRuntime.awrap(client.db("data").collection("ping-data").insertOne(ping_data)["catch"](function (err) {
+                      console.error(err);
+                    }));
 
-        case 5:
-          _context.next = 7;
-          return regeneratorRuntime.awrap(client.close());
+                  case 6:
+                  case "end":
+                    return _context.stop();
+                }
+              }
+            });
+          });
+          setTimeout(main, x * 1000);
 
-        case 7:
+        case 8:
         case "end":
-          return _context.stop();
+          return _context2.stop();
       }
     }
   });
-});
+}
+
+main();
--- frontend/ping.js
+++ frontend/ping.js
@@ -3,18 +3,6 @@ var moment = require('moment-timezone');
 const { MongoClient } = require('mongodb');
 
 
-
-//date
-var time_utc = moment().tz("UTC").format('LTS')
-var date_utc = moment().tz("UTC").format('L')
-
-var time_est = moment().format('LTS')
-var date_est = moment().format('L')
-
-var _id = `${date_est} ${time_est}`
-console.log(_id)
-
-
 //configure mongoDB
 const { password, uri } = require("../config.json")
 const client = new MongoClient(uri, {
@@ -24,23 +12,45 @@ const client = new MongoClient(uri, {
 
 var host = '51.161.117.73'
 
-tcpp.ping({ address: host, port: 25677 }, async(err, data) => {
+async function main() {
+    x = 5
+    var time_utc = moment().tz("UTC").format('LTS')
+    var date_utc = moment().tz("UTC").format('L')
 
-    ping_data = {
-        "_id": _id,
-        "day": moment().format('ddd'),
-        "hour": moment().format('hh'),
-        "device": "the-beast",
-        "date_est": date_est,
-        "time_est": time_est,
-        "date_utc": date_utc,
-        "time_utc": time_utc,
-        "ip": data.address,
-        "ping_avg": data.avg,
-        "ping_max": data.max,
-        "ping_min": data.min
-    }
-    await client.connect()
-    await client.db("data").collection("ping-data").insertOne(ping_data);
-    await client.close()
-});
+    var time_est = moment().format('LTS')
+    var date_est = moment().format('L')
+
+    var _id = `${date_est} ${time_est}`
+
+    tcpp.ping({ address: host, port: 25677 }, async(err, data) => {
+        console.log(_id)
+
+        
+
+        ping_data = {
+            "_id": _id,
+            "day": moment().format('ddd'),
+            "hour": moment().format('hh'),
+            "device": "the-beast",
+            "date_est": date_est,
+            "time_est": time_est,
+            "date_utc": date_utc,
+            "time_utc": time_utc,
+            "ip": data.address,
+            "ping_avg": data.avg,
+            "ping_max": data.max,
+            "ping_min": data.min
+        }
+        await client.connect()
+        await client.db("data").collection("ping-data").insertOne(ping_data).catch( (err) => {
+            console.error(err)
+        });
+
+
+    });
+    setTimeout(main, x*1000);
+
+}
+
+
+main()
